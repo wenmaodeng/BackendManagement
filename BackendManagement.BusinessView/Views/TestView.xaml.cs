4932890c75a2e81d0178f8e9f35344fa388f1aa4
dengwenmao <wenmaodeng@outlook.com>
2024-04-15 16:59:37 +0800

    引用ScottPlot.WPF

--- BackendManagement.BusinessView/Views/TestView.xaml.cs
+++ BackendManagement.BusinessView/Views/TestView.xaml.cs
@@ -23,6 +23,10 @@ namespace BackendManagement.BusinessView.Views
         public TestView()
         {
             InitializeComponent();
+            double[] dataX = { 1, 2, 3, 4, 5 };
+            double[] dataY = { 1, 4, 9, 16, 25 };
+            WpfPlot1.Plot.Add.Scatter(dataX, dataY);
+            WpfPlot1.Refresh();
         }
     }
 }
